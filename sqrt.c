f42d41191a54b3218b8f1c07b29f9c9859ece72e
keerthi <system@keerthi>
2020-10-16 12:55:52 +0530

    Added if condition.

--- sqrt.c
+++ sqrt.c
@@ -13,8 +13,13 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
-	printf("Sqrt of %d is %f\n",input,sqrt(input));
-        printf("End of program. Exiting.");
+        if(input < 0) {
+        	printf("invalid input\n");
+        }
+        else{
+		printf("Sqrt of %d is %f\n",input,sqrt(input));
+        }
+	printf("End of program. Exiting.");
 	return(0);
 
 } // end main
